6151352612ec7d88315e489222352df509641b88
Ssuuna clive <cloveclive8@gmail.com>
2024-09-25 15:41:01 +0300

    Add files via upload

---
+++ ForLoop/ForLoop/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    ' Event handler for button click
+    Private Sub btnShowNumbers_Click(sender As Object, e As EventArgs) Handles btnShowNumbers.Click
+        ' Clear previous content
+        lblNumbers.Text = ""
+
+        ' For loop to display numbers from 1 to 10
+        For i As Integer = 1 To 10
+            lblNumbers.Text &= i.ToString() & " "  ' Append each number to the label with a space
+        Next
+    End Sub
+End Class
